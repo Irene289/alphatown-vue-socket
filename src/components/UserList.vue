4f0d9772a17bb3a6dbdf0b6e152abc46b35e3403
SeijoHuang <erinhuang817@gmail.com>
2022-05-28 16:48:56 +0800

    add 新增元件檔案

---
+++ src/components/UserList.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="user__list">
+    <h1> list</h1>
+  </div>
+</template>
---
+++ src/views/Chat.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="container">
+    <div>
+      <UserList />
+    </div>
+     <div>
+      <UserChat />
+    </div>
+
+  </div>
+</template>
+<script>
+import UserList from '../components/UserList.vue'
+import UserChat from '../components/UserChat.vue'
+export default {
+  name:'Chat',
+  components:{
+    UserList,
+    UserChat
+  }
+  
+}
+</script>
