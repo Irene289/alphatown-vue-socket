4cda2209198a3e350cee5a11f4f12308b1d143b9
SeijoHuang <erinhuang817@gmail.com>
2022-05-28 22:49:54 +0800

    add Title component and style

--- src/components/Title.vue
+++ src/components/Title.vue
@@ -1,10 +1,18 @@
 <template>
-  <div class="title-container">
-    <slot name="title">
-      <p class=title__content></p>
-    </slot>
+  <div class="title">   
+      <div class=title__content>
+        <slot name="title"/>
+      </div>
+   
   </div>
 </template>
 <style lang="scss" scoped>
-
+ @import "../assets/scss/_basic.scss";
+  .title__content{
+    padding: 24px;
+    height: 80px;
+    font-size: 24px;;
+    font-weight: 700;
+    border-bottom: 1px solid $border-grey;
+  }
 </style>
